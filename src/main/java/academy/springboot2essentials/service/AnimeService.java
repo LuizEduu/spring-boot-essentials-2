dc9c96bcdec2a2b71174b1b2f5b9fd24755a3de7
Luiz Eduardo <l.eduardo8316@gmail.com>
2022-02-05 18:38:07 -0300

    feat: add get anime by id method

--- src/main/java/academy/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/springboot2essentials/service/AnimeService.java
@@ -1,13 +1,23 @@
 package academy.springboot2essentials.service;
 
 import academy.springboot2essentials.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+
+    private final List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+
+    public Anime findById(long id) {
+        return animes.stream().filter(anime -> anime.getId().equals(id)).
+                findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
     public List<Anime> listAll() {
-     return List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+        return animes;
     }
 }
