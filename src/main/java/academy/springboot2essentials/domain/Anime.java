a1b4dbf7c7afc8315b9469584b7c20831016714e
Luiz Eduardo <l.eduardo8316@gmail.com>
2022-02-06 17:20:21 -0300

    feat: add mysqlConnection

--- src/main/java/academy/springboot2essentials/domain/Anime.java
+++ src/main/java/academy/springboot2essentials/domain/Anime.java
@@ -1,12 +1,34 @@
 package academy.springboot2essentials.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
-@Data
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Anime anime = (Anime) o;
+        return id.equals(anime.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/academy/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/springboot2essentials/service/AnimeService.java
@@ -5,19 +5,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
 
-    private final List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk"));
+    private static final List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Berserk")));
+    }
 
     public Anime findById(long id) {
-        return animes.stream().filter(anime -> anime.getId().equals(id)).
-                findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+        return animes.stream().filter(anime -> anime.getId().equals(id)).findFirst().orElseThrow
+                (() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 
     public List<Anime> listAll() {
         return animes;
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(long id) {
+        animes.remove(findById(id));
+    }
+
+    public void replace(Anime anime, long id) {
+        delete(id);
+        anime.setId(id);
+        animes.add(anime);
     }
 }
