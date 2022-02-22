440da20beef5a29e10e78cdea4277b9b4249d53c
Luiz Eduardo <l.eduardo8316@gmail.com>
2022-02-22 18:02:28 -0300

    feat: add pagination to list animes

--- src/main/java/academy/springboot2essentials/service/AnimeService.java
+++ src/main/java/academy/springboot2essentials/service/AnimeService.java
@@ -7,6 +7,8 @@ import academy.springboot2essentials.repository.AnimeRepository;
 import academy.springboot2essentials.requests.AnimePostRequestBody;
 import academy.springboot2essentials.requests.AnimePutRequestBody;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,8 +24,8 @@ public class AnimeService {
         return animeRepository.findById(id).orElseThrow(() -> new BadRequestExecption("Anime not found"));
     }
 
-    public List<Anime> listAll() {
-        return animeRepository.findAll();
+    public Page<Anime> listAll(Pageable pageable) {
+        return animeRepository.findAll(pageable);
     }
 
     public List<Anime> findByName(String name) {
